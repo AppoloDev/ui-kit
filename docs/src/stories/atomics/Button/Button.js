d7b5b1f4bcb25c5d1c1542a77c1636896cee55b1
Fredxd <frederic.baudouin.45@gmail.com>
2021-02-19 09:53:47 +0100

    feat(button): finalize button component

--- docs/src/stories/atomics/Button/Button.js
+++ docs/src/stories/atomics/Button/Button.js
@@ -17,28 +17,61 @@ const config = {
             },
         },
         size: {
+            name: 'Taille',
             control: {
                 type: 'select',
                 options: [
                     'default',
+                    'small',
                     'medium',
+                    'large',
                     'block'
                 ],
             },
         },
         type: {
+            name: 'Variation',
             control: {
                 type: 'select',
                 options: [
                     'default',
                     'outline',
-                    'ghost'
+                    'ghost',
+                    'link'
+                ],
+            },
+        },
+        iconPosition: {
+            name: 'Position de l\'icône',
+            control: {
+                type: 'select',
+                options: [
+                    'default',
+                    'left',
+                    'right',
+                ],
+            },
+        },
+        icon: {
+            name: 'Icône',
+            control: {
+                type: 'select',
+                options: [
+                    'svg',
+                    'cssgg',
                 ],
             },
         },
         disabled: {
+            name: 'Statut désactivé',
             control: {
                 type: 'boolean'
+            },
+        },
+        label: {
+            name: 'Libellé',
+            control: {
+                type: 'text'
             },
         },
     }
@@ -47,11 +80,41 @@ const config = {
 const Template = (args) => {
     const color = `btn-${args.color}`;
     const size = args.size !== 'default' ? ` btn-${args.size}` : '';
-    const type = args.type !== 'default' ? ` btn-${args.type}-${args.color}` : '';
+    const type = args.type !== 'default' ? ` btn-${args.type}` : '';
+    const icon = args.iconPosition !== 'default' ? ` btn-icon-${args.iconPosition}` : '';
+
+    const renderSvg = function () {
+        return (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     className="feather feather-plus">
+            <line x1="12" y1="5" x2="12" y2="19"></line>
+            <line x1="5" y1="12" x2="19" y2="12"></line>
+        </svg>);
+    }
+
+    const renderCssGGIcon = function () {
+        return (<i className="gg-math-plus"></i>);
+    }
+
+    let iconLeft = null;
+    let iconRight = null;
+
+    if (args.iconPosition === 'left' && args.icon === 'svg') {
+        iconLeft = renderSvg();
+    } else if (args.iconPosition === 'right' && args.icon === 'svg') {
+        iconRight = renderSvg();
+    } else if (args.iconPosition === 'left' && args.icon === 'cssgg') {
+        iconLeft = renderCssGGIcon();
+    } else if (args.iconPosition === 'right' && args.icon === 'cssgg') {
+        iconRight = renderCssGGIcon();
+    }
+
     return (
         <>
-            <button class={`btn ${color}${size}${type}`} disabled={args.disabled}>
+            <button class={`btn ${color}${size}${type}${icon}`} disabled={args.disabled}>
+                {iconLeft}
                 {args.label}
+                {iconRight}
             </button>
         </>
 
