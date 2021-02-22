ff8586a62bcc3d653233797f76a99cee5033ad40
Fredxd <frederic.baudouin.45@gmail.com>
2021-02-22 15:04:09 +0100

    feat(badge): doc enhancement

--- docs/src/stories/atomics/Badge/Badge.stories.js
+++ docs/src/stories/atomics/Badge/Badge.stories.js
@@ -33,14 +33,30 @@ export default {
             page: () => (
                 <>
                     <Title/>
-                    <Description>Documentation for badge component</Description>
-                    <Description>You can import div with following</Description>
+                    <Description markdown={dedent`
+                        ## Import
+                        You can import badge style with following  
+                    `}/>
                     <Source
                         language='scss'
                         code={dedent`
                             @import('~uikit/atomics/badge/badge')
                          `}
                     />
+                    <Description markdown={dedent`
+                        ## Variables
+                        You can override the following CSS variables to change the style of the badges
+                        
+                        | CSS Variable                        | Default value                  |
+                        | ----------------------------------- |:------------------------------:|
+                        | --badge-vertical-padding            | 8px                            |
+                        | --badge-horizontal-padding          | 8px                            |
+                        | --badge-background-color            | var(--color-primary-500)       |
+                        | --badge-color                       | var(--color-primary-100)       |
+                        | --badge-border-radius               | 4px                            |
+                       
+                        ## Playground
+                    `}/>
                     <Primary/>
                     <ArgsTable story={PRIMARY_STORY}/>
                     <Stories/>
